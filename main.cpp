db7a0976e0c4a730f051d035bdc3933f347de9e2
Gabriele-Granvillano <114691186+Gabriele-Granvillano@users.noreply.github.com>
2022-12-21 12:14:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,49 +10,38 @@ float f(float a)
 bool verifica(float a, float b)
 {
     if (f(a)*f(b)>=0)
-    {
         return true;
-    }
     else
-    {
         return false;
-    }
 }
 
 int main()
 {
     float a,b;
+
     do
     {
         cout << "inserire estremi" << endl;
         cin >> a >> b;
     }
-    while (verifica(a,b));
+    while(verifica(a,b));
     float x;
     float err;
     do
     {
         x=(a+b)/2;
         if (f(x)==0)
-        {
             break;
-        }
         else
         {
             if (!verifica(a, x))
-            {
                 b = x;
-            }
             else
-            {
                 a = x;
-                err = abs((b - a) / 2);
-            }
+            err = abs((b - a) / 2);
         }
     }
     while (err>=1e-6);
-    {
-        cout << int(x*10000)/10000.0 << endl;
-    }
+    cout << int(x*10000)/10000.0 << endl;
     return 0;
 }
